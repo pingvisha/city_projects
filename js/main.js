cf59342cc3fe4438bd6e193da7b140b6f972aa2b
pingvin <r-n-b@bk.ru>
2014-02-24 19:20:00 +0400

    filial city page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 /*http://pingvisha.ru*/
+function map (address) {
+    return function init() {
+        var myMap = new ymaps.Map('map', {
+            center: [55.753994, 37.622093],
+            zoom: 9,
+            behaviors: ['default', 'scrollZoom']
+        });
+        ymaps.geocode(address, {
+            results: 1
+        }).then(function (res) {
+                var firstGeoObject = res.geoObjects.get(0),
+                    coords = firstGeoObject.geometry.getCoordinates(),
+                    bounds = firstGeoObject.properties.get('boundedBy');
+                myMap.geoObjects.add(firstGeoObject);
+                myMap.setBounds(bounds, {
+                    checkZoomRange: true
+                });
+            });
+    }
+}
 $(document).ready(function() {
-
 });
