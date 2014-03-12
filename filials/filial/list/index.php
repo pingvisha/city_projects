0b38dd8c524f0c941ae0874cdd8a5d92eac168cf
pingvin <r-n-b@bk.ru>
2014-03-12 14:45:49 +0400

    map fix

---
+++ filials/filial/list/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Pingvin
+ * Date: 12.03.14
+ * Time: 13:22
+ * To change this template use File | Settings | File Templates.
+ */
+echo '
+[{"name":"\u041c\u043e\u0441\u043a\u0432\u0430","loc":{"lat":55.76162,"lng":37.594473},"alias":"moscow","active":1,"cnt":500},{"name":"\u0420\u043e\u0441\u0442\u043e\u0432-\u043d\u0430-\u0414\u043e\u043d\u0443","loc":{"lat":47.2610085,"lng":39.6279999},"alias":"rostov-na-donu","active":0,"cnt":500},{"name":"\u0421\u0430\u043d\u043a\u0442-\u041f\u0435\u0442\u0435\u0440\u0431\u0443\u0440\u0433","loc":{"lat":59.939192,"lng":30.335945},"alias":"spb","active":0,"cnt":500},{"name":"\u041d\u043e\u0432\u043e\u0441\u0438\u0431\u0438\u0440\u0441\u043a","loc":{"lat":55.015426,"lng":82.934203},"alias":"novosib","active":0,"cnt":500},{"name":"\u0415\u043a\u0430\u0442\u0435\u0440\u0438\u043d\u0431\u0443\u0440\u0433","loc":{"lat":56.832447,"lng":60.604491},"alias":"ekb","active":0,"cnt":500},{"name":"\u041d\u0438\u0436\u043d\u0438\u0439 \u041d\u043e\u0432\u0433\u043e\u0440\u043e\u0434","loc":{"lat":56.322629,"lng":43.986297},"alias":"nnovgorod","active":0,"cnt":500},{"name":"\u041a\u0430\u0437\u0430\u043d\u044c","loc":{"lat":55.798965,"lng":49.105346},"alias":"kazan","active":0,"cnt":500},{"name":"\u0421\u0430\u043c\u0430\u0440\u0430","loc":{"lat":53.232345,"lng":50.161986},"alias":"samara","active":0,"cnt":24},{"name":"\u041e\u043c\u0441\u043a","loc":{"lat":54.96106,"lng":73.387084},"alias":"omsk","active":0,"cnt":500},{"name":"\u0412\u043e\u043b\u0433\u043e\u0433\u0440\u0430\u0434","loc":{"lat":48.701838,"lng":44.51364},"alias":"volgograd","active":0,"cnt":500},{"name":"\u041a\u0440\u0430\u0441\u043d\u043e\u044f\u0440\u0441\u043a","loc":{"lat":56.010283,"lng":92.923629},"alias":"krasnoyarsk","active":0,"cnt":500},{"name":"\u041f\u0435\u0440\u043c\u044c","loc":{"lat":58.008825,"lng":56.244292},"alias":"perm","active":0,"cnt":500}]';
